8720d1eca48d1e8ccf189bfe2a3cb7ff66598e3d
Catgirlleeroy <leeroypieterse7@gmail.com>
2025-11-24 13:07:53 +0100

    Create School les 2.py

---
+++ school/School les 2.py
@@ -0,0 +1,27 @@
+naam = "Sara"
+leeftijd = "17"
+kleur = "blauw"
+print("Hoi,ik ben", naam, "Ik ben", leeftijd, "en mijn lievelingskleur is", kleur)
+
+a = 10 + 5
+b = 3 + 2
+print(a + b)
+
+x = 8
+y = x + 4
+getallen = y * 3
+print(getallen)
+
+voornaam = "Sam"
+achternaam = "Jansen"
+print(voornaam ,achternaam)
+
+drank = 1.50
+broodje = 3.20
+reep  = 1.10
+appel = 0.50
+print(drank + broodje + reep + appel)
+
+prijs = 25.00
+korting = 5.00
+print(prijs - korting)
