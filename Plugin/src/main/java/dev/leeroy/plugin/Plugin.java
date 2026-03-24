d24434fd404a2eb65029e54280da9ea9b6c5375a
Catgirlleeroy <leeroypieterse7@gmail.com>
2026-03-25 00:55:52 +0100

    Added Unban command

--- Plugin/src/main/java/dev/leeroy/plugin/Plugin.java
+++ Plugin/src/main/java/dev/leeroy/plugin/Plugin.java
@@ -11,24 +11,31 @@ public final class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Ban system
         banManager = new BanManager(this);
 
+        // Heal command
         getCommand("heal").setExecutor(new HealCommand());
 
+        // Gamemode shortcut commands
         Gamemodes gamemodesExecutor = new Gamemodes();
         getCommand("gm0").setExecutor(gamemodesExecutor);
         getCommand("gm1").setExecutor(gamemodesExecutor);
         getCommand("gm2").setExecutor(gamemodesExecutor);
         getCommand("gm3").setExecutor(gamemodesExecutor);
 
+        // Ban commands
         getCommand("ban").setExecutor(new BanCommand(banManager));
         getCommand("tempban").setExecutor(new TempBanCommand(banManager));
         getCommand("checkban").setExecutor(new CheckBanCommand(banManager));
+        getCommand("unban").setExecutor(new UnbanCommand(banManager));
 
+        // Listeners
         getServer().getPluginManager().registerEvents(new BanListener(banManager), this);
     }
 
     @Override
     public void onDisable() {
+        // Plugin shutdown logic
     }
 }
---
+++ Plugin/src/main/java/dev/leeroy/plugin/commands/UnbanCommand.java
@@ -0,0 +1,41 @@
+package dev.leeroy.plugin.commands;
+
+import dev.leeroy.plugin.Utils.BanManager;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class UnbanCommand implements CommandExecutor {
+
+    private final BanManager banManager;
+
+    public UnbanCommand(BanManager banManager) {
+        this.banManager = banManager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!sender.hasPermission("bansystem.unban")) {
+            sender.sendMessage(ChatColor.RED + "You don't have permission to unban players.");
+            return true;
+        }
+
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.YELLOW + "Usage: /unban <player>");
+            return true;
+        }
+
+        String targetName = args[0];
+
+        if (!banManager.isBanned(targetName)) {
+            sender.sendMessage(ChatColor.RED + targetName + " is not currently banned.");
+            return true;
+        }
+
+        banManager.unban(targetName);
+        sender.sendMessage(ChatColor.GREEN + targetName + " has been unbanned.");
+        return true;
+    }
+}
