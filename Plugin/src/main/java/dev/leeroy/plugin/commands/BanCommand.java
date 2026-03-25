fbced4856ecc322aee86ab9d39ca44a7db6898b6
Catgirlleeroy <leeroypieterse7@gmail.com>
2026-03-25 01:24:10 +0100

    Updated Ban command added Sounds and ligtning

--- Plugin/src/main/java/dev/leeroy/plugin/commands/BanCommand.java
+++ Plugin/src/main/java/dev/leeroy/plugin/commands/BanCommand.java
@@ -3,6 +3,7 @@ package dev.leeroy.plugin.commands;
 import dev.leeroy.plugin.Utils.BanManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -41,15 +42,31 @@ public class BanCommand implements CommandExecutor {
 
         banManager.ban(targetName, reason, sender.getName());
 
+        // Effects & kick if online
         Player target = Bukkit.getPlayerExact(targetName);
         if (target != null) {
+            // Lightning strike at their location
+            target.getWorld().strikeLightning(target.getLocation());
+
+            // Wither death sound heard by everyone on the server
+            Bukkit.getOnlinePlayers().forEach(p ->
+                    p.playSound(p.getLocation(), Sound.ENTITY_WITHER_DEATH, 1.0f, 1.0f)
+            );
+
             target.kickPlayer(
                     ChatColor.RED + "You have been permanently banned.\n" +
                             ChatColor.WHITE + "Reason: " + reason
             );
         }
 
-        sender.sendMessage(ChatColor.GREEN + "Banned " + targetName + ". Reason: " + reason);
+        // Public broadcast to all players
+        Bukkit.broadcastMessage(
+                ChatColor.RED + "[BAN] " +
+                        ChatColor.YELLOW + targetName +
+                        ChatColor.RED + " has been permanently banned! " +
+                        ChatColor.GRAY + "Reason: " + reason
+        );
+
         return true;
     }
 }
--- Plugin/src/main/java/dev/leeroy/plugin/commands/TempBanCommand.java
+++ Plugin/src/main/java/dev/leeroy/plugin/commands/TempBanCommand.java
@@ -3,6 +3,7 @@ package dev.leeroy.plugin.commands;
 import dev.leeroy.plugin.Utils.BanManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -51,9 +52,17 @@ public class TempBanCommand implements CommandExecutor {
 
         banManager.tempBan(targetName, reason, sender.getName(), durationMs);
 
-        // Kick if online
+        // Effects & kick if online
         Player target = Bukkit.getPlayerExact(targetName);
         if (target != null) {
+            // Lightning strike at their location
+            target.getWorld().strikeLightning(target.getLocation());
+
+            // Wither death sound heard by everyone on the server
+            Bukkit.getOnlinePlayers().forEach(p ->
+                    p.playSound(p.getLocation(), Sound.ENTITY_WITHER_DEATH, 1.0f, 1.0f)
+            );
+
             target.kickPlayer(
                     ChatColor.RED + "You have been temporarily banned.\n" +
                             ChatColor.WHITE + "Duration: " + BanManager.formatRemaining(System.currentTimeMillis() + durationMs) + "\n" +
@@ -61,8 +70,14 @@ public class TempBanCommand implements CommandExecutor {
             );
         }
 
-        sender.sendMessage(ChatColor.GREEN + "Tempbanned " + targetName
-                + " for " + durationStr + ". Reason: " + reason);
+        // Public broadcast to all players
+        Bukkit.broadcastMessage(
+                ChatColor.RED + "[TEMPBAN] " +
+                        ChatColor.YELLOW + targetName +
+                        ChatColor.RED + " has been temporarily banned! " +
+                        ChatColor.GRAY + "Duration: " + durationStr + " | Reason: " + reason
+        );
+
         return true;
     }
 }
